d1feb67e8a9dc411fb5c060416118bda73f729f7
Xiaokai Liu <JohannesLiu@live.com>
2023-03-28 18:13:51 +0800

    update

--- deploy.py
+++ deploy.py
@@ -6,7 +6,7 @@ LastEditors: Johannes Liu
 email: iexkliu@gmail.com
 github: https://github.com/johannesliu
 Date: 2023-03-28 18:10:39
-LastEditTime: 2023-03-28 18:11:46
+LastEditTime: 2023-03-28 18:13:37
 motto: Still water run deep
 Description: Modify here please
 FilePath: \ReDevOps\deploy.py
@@ -17,5 +17,5 @@ import os
 os.system("mdbook build")
 
 os.system("git add -A")
-os.system('git commit "update"')
+os.system('git commit -m "update"')
 os.system("git push")
